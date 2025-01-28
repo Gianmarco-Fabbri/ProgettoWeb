9949c350d67d90575a41130c961d92edaa21e4af
gimbo <gianmarco.fabbri3@studio.unibo.it>
2025-01-28 13:47:14 +0100

    registrazione

--- js/displayPassword.js
+++ js/displayPassword.js
@@ -1,4 +1,12 @@
-function toggleVisibility(fieldId) {
-    const field = document.getElementById(fieldId);
-    field.type = field.type === 'password' ? 'text' : 'password';
+function toggleVisibility(inputId, imgId) {
+    const input = document.getElementById(inputId);
+    const img = document.getElementById(imgId);
+
+    if (input.type === "password") {
+        input.type = "text";
+        img.src = "../img/eye_open.png";
+    } else {
+        input.type = "password";
+        img.src = "../img/eye_close.png";
+    }
 }
