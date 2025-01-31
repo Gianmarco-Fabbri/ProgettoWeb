1cd761a7d23c9348131ba734830e8eac2140290d
Kevin Shimaj <kevin.shimaj@studio.unibo.it>
2025-01-31 19:24:26 +0100

    little fix

--- ajax/api-kit_consigliati.php
+++ ajax/api-kit_consigliati.php
@@ -1,7 +1,13 @@
 <?php
-require_once('bootstrap.php') /*percorso relativo a chi lo chiama (index.php in questo caso)*/
+    // require_once('bootstrap.php'); /percorso relativo a chi lo chiama (index.php in questo caso)/
 
-$kits = $dbh->getAllKits();
-header("Content-Type: application/json"); /*definisco i tipo di dati da ritornare*/
-echo json_encode($kits); /*ritorno un json*/
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
+    echo "Prova";
+
+    // $kits = $dbh->getAllKits();
+    // header("Content-Type: application/json"); /definisco i tipo di dati da ritornare/
+    // echo json_encode($kits); /ritorno un json/
 ?>
