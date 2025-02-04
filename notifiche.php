13aeba111107c8b127af3136cec662cd542e6d04
Kevin Shimaj <kevin.shimaj@studio.unibo.it>
2025-02-04 12:37:03 +0100

    testing

--- notifiche.php
+++ notifiche.php
@@ -1,10 +1,6 @@
 <?php
 require_once 'bootstrap.php';
 
-/* ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL); */
-
 $templateParams["titolo"] = "Notifiche - Benessere market";
 $templateParams["nome"] = "template/notifiche_main.php";
 $templateParams["js"] = ["js/notifiche.js"];
