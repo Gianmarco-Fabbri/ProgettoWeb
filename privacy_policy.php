65b192dfda5daf5cc676916feba47ee961079c53
gimbo <gianmarco.fabbri3@studio.unibo.it>
2025-02-04 10:53:57 +0100

    fix log venditore e php mancanti

---
+++ privacy_policy.php
@@ -0,0 +1,9 @@
+<?php
+require_once 'bootstrap.php';
+
+$templateParams["titolo"] = "Privacy Policy | Benessere market";
+$templateParams["nome"] = "privacy_policy_main.php";
+$templateParams["navs"] = [["link" => "privacy_policy.php", "name" => "Privacy Policy"]];
+
+require 'template/base.php';
+?>
